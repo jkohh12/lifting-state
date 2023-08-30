df39789f31a236b024edf393fc1b746035437878
jkohh12 <jkohh.12@gmail.com>
2023-08-30 04:57:08 -0700

    hook form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styles from './components/styles.module.css'
 
-function App() {
+const App = (props) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
+
+  const createUser = (e) => {
+    //prevent default refresh of browser to keep state from being reset
+    e.preventDefault();
+
+    //javascript obj to hold all vals
+    const newUser = {username, email, password, confirmPassword};
+    console.log("Welcome", newUser);
+    //used to reset info to empty string
+    setUsername("");
+    setEmail("");
+    setPassword("");
+    setconfirmPassword("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className = "App">
+      <form onSubmit= { createUser }>
+
+          <div className= {styles.border}>
+            <label>Username: </label>
+            <input type = "text" value = {username} /*setting username here */ onChange= { (e) => setUsername(e.target.value) /*reset username to "" here */ } /> 
+          </div>
+          <br>
+          </br>
+          <div className= {styles.border}>
+            <label>Email Address: </label>
+            <input type = "text" value = {email} onChange = { (e) => setEmail(e.target.value) } />
+          </div>
+          <br>
+          </br>
+          <div  className= {styles.border}>
+            <label>Password: </label>
+            <input type = "text" value = {password} onChange = { (e) => setPassword(e.target.value) } />  
+          </div>
+          <br>
+          </br>
+          <div className = {styles.border}>
+            <label>Confirm Password: </label>
+            <input type = "text" value = {confirmPassword} onChange = { (e) => setconfirmPassword(e.target.value) } />  
+          </div>
+          <br>
+          </br>
+          <input type= "submit" value = "Create User" />
+      </form>
+  
+    
     </div>
+    <div className = "result">
+      Username: {username}
+      <br>
+      </br>
+      Email: {email}
+      <br>
+      </br>
+      Password: {password}
+      <br>
+      </br>
+      Confirm Password: {confirmPassword}
+
+    </div>
+    </>
+
+    
   );
 }
 
